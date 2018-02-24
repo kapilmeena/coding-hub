0e3f7ab3c2126e1ff99429a5a0fef589d1f53258
Anshumaan Yadav <anshumaanyadav@gmail.com>
2018-02-25 00:50:38 +0530

    add maps, payment info, Fix Timer Bug :P, Zoo-Zoo <3

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
+//cookie stuff                
+let x = document.cookie;
 //jQuery Stuff
 $(document).ready(
     function() {
 
         //Go btn-animation
+        if(x.includes("alreadyVisited=True")){
+            $('#landing-page').css('display','none');
+        }
+        else{
+            document.cookie = "alreadyVisited=True";
+        }
         $('#go-btn').on('click',
             function() {
                 $('#landing-page').slideUp(1200);
@@ -20,69 +28,59 @@ $(document).ready(
                             setTimeout(function(i) {
                                     $(val).addClass("anime");
                                 },
-                                i * 700);
+                                i * 800);
                         });
                     }
-                ,1000);
-            }
-        );
-
-        //navigation custom active class
-        $('#main-navbar li').on('click',
-            function() {
-                if (!$(this).hasClass('active')) {
-                    $('#main-navbar li.active').removeClass('active').children('span').remove();
-                    $(this).addClass('active').append('<span class="sr-only">(current)</span>');
-                    target = $(this).children('a').attr('href');
-                    $('html, body').animate({
-                        scrollTop:$(target).offset().top
-                    }, 500);
-                    $('.navbar-toggle').click();
-                }
+                ,500);
             }
         );
     }
 );
 
 //timer-js
-function getTimeRemaining(endtime) {
-    var t = Date.parse(endtime) - Date.parse(new Date());
-    var seconds = Math.floor((t / 1000) % 60);
-    var minutes = Math.floor((t / 1000 / 60) % 60);
-    var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
-    var days = Math.floor(t / (1000 * 60 * 60 * 24));
-    return {
-        'total': t,
-        'days': days,
-        'hours': hours,
-        'minutes': minutes,
-        'seconds': seconds
-    };
-}
-
-function initializeClock(id, endtime) {
-    var clock = document.getElementById(id);
-    var daysSpan = clock.querySelector('.days');
-    var hoursSpan = clock.querySelector('.hours');
-    var minutesSpan = clock.querySelector('.minutes');
-    var secondsSpan = clock.querySelector('.seconds');
-
-    function updateClock() {
-        var t = getTimeRemaining(endtime);
-
-        daysSpan.innerHTML = t.days;
-        hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
-        minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
-        secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
-
-        if (t.total <= 0) {
-            clearInterval(timeinterval);
-        }
+function timer(){
+    function getTimeRemaining(endtime) {
+        var t = Date.parse(endtime) - Date.parse(new Date());
+        var seconds = Math.floor((t / 1000) % 60);
+        var minutes = Math.floor((t / 1000 / 60) % 60);
+        var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+        var days = Math.floor(t / (1000 * 60 * 60 * 24));
+        return {
+            'total': t,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
     }
 
-    updateClock();
-    var timeinterval = setInterval(updateClock, 1000);
-}
+    function initializeClock(id, endtime) {
+        var clock = document.getElementById(id);
+        var daysSpan = clock.querySelector('.days');
+        var hoursSpan = clock.querySelector('.hours');
+        var minutesSpan = clock.querySelector('.minutes');
+        var secondsSpan = clock.querySelector('.seconds');
 
-var deadline = new Date(2018, 1, 22);
-initializeClock('clockdiv', deadline);
+        function updateClock() {
+            var t = getTimeRemaining(endtime);
+
+            if (t.total>0){
+                daysSpan.innerHTML = t.days;
+                hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+                minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+                secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+            }
+            else{
+                document.getElementById(id).style.display = 'none';
+                return;
+            }
+        }
+
+        updateClock();
+        var timeinterval = setInterval(updateClock, 1000);
+    }
+    
+    var deadline = new Date(2018, 2, 9);
+    initializeClock('clockdiv', deadline);
+}
+timer();
